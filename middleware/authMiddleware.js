edd05369440dd0a705de3431c8bf7e67c91838ce
Janette Campbell <campbjp@hotmail.com>
2022-07-19 23:18:55 -0400

    feat: added req.user = decoded

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
+    req.user = decoded;
     console.log(decoded);
 
     next();
